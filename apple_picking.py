cb6239e3f3df107aa66009d1c8a8971bd642b3bf
Alexander You <youa@oregonstate.edu>
2019-11-21 16:51:41 -0800

    some qol improvements, added plots of outputs

--- apple_picking.py
+++ apple_picking.py
@@ -15,12 +15,19 @@ import matplotlib.pyplot as plt
 
 import os
 
+ROOT = os.path.dirname(os.path.realpath(__file__))
+
 class ApplePicking:
-    def __init__(self, data_dir="/home/hmt_user/ROB_537_LBC/project/all_grasp_data/", model_dir='./Models/', test_split=0.05, window_size=10):
-        self.data_dir = data_dir
-        self. model_dir = model_dir
+
+    INPUT_COLS = ['force_mag', '/manipulator_pose.x', '/manipulator_pose.y', '/manipulator_pose.z', '/manipulator_pose.rx', '/manipulator_pose.ry', '/manipulator_pose.rz', '/manipulator_pose.rw']
+    OUTPUT_COLS = ['/wrench.fx', '/wrench.fy', '/wrench.fz']
+
+    def __init__(self, data_folder = 'all_grasp_data', model_folder = 'models', test_split=0.05, window_size=10):
+        self.data_dir = os.path.join(ROOT, data_folder)
+        self.model_dir = os.path.join(ROOT, model_folder)
         self.window_size = window_size
-        self.train, self.test = self.load_data(test_split)
+        self.train, self.test = self.load_all_data(test_split)
+
 
     def add_force_mag_data(self, row):
         fx = row['/wrench.fx']
@@ -29,17 +36,22 @@ class ApplePicking:
         f_mag = math.sqrt(fx**2 + fy**2 + fz**2)
         return f_mag
 
+    def adjust_force_data(self, df):
+        force_cols = ['/wrench.fx', '/wrench.fy', '/wrench.fz']
+        subset = df[force_cols]
+        tared = subset - subset.iloc[0]
+        df[force_cols] = tared
+        df['force_mag'] = (tared ** 2).sum(axis=1) ** 0.5
+
     def format_data(self, df):
-        input_col = ['force_mag', '/manipulator_pose.x', '/manipulator_pose.y', '/manipulator_pose.z', '/manipulator_pose.rx', '/manipulator_pose.ry', '/manipulator_pose.rz', '/manipulator_pose.rw']
-        output_col = ['/wrench.fx', '/wrench.fy', '/wrench.fz']
         n_df = len(df) - self.window_size
         X = []
         Y = []
         for i in range(0, n_df):
             seq_X = df.iloc[i: i+self.window_size]
             seq_Y = seq_X.iloc[-1]
-            x = seq_X[input_col].values
-            y = seq_Y[output_col].values
+            x = seq_X[self.INPUT_COLS].values
+            y = seq_Y[self.OUTPUT_COLS].values
             X.append(x)
             Y.append(y)
         return X, Y
@@ -47,12 +59,12 @@ class ApplePicking:
     def get_data(self, files_list):
         X_data = []
         Y_data = []
+
+        print('Loading data...')
         for file in files_list:
             temp_df = pd.read_csv(file)
+            self.adjust_force_data(temp_df)
 
-            temp_df = temp_df - temp_df.iloc[0].values.squeeze()
-            temp_df = temp_df.loc[1:]
-            temp_df['force_mag'] = temp_df.apply(lambda row: self.add_force_mag_data(row), axis=1)
 
             temp_X, temp_Y = self.format_data(temp_df)
             X_data = X_data + temp_X
@@ -62,9 +74,10 @@ class ApplePicking:
         Y_data = np.array(Y_data, dtype=np.float64)
         return X_data, Y_data
 
-    def load_data(self, test_split):
+    def load_all_data(self, test_split):
         print ("Loading Data ...")
-        all_files = [file for file in glob.glob(self.data_dir + "*.csv", recursive=False)]
+
+        all_files = [file for file in glob.glob(os.path.join(self.data_dir, "*.csv"), recursive=False)]
         random.shuffle(all_files)
 
         split_idx = int(round(test_split*len(all_files)))
@@ -104,16 +117,16 @@ class ApplePicking:
         feature_dim = len(input_col)
 
         model_name = 'force_vec_pred_ws' + str(self.window_size) + '_featdim' + str(feature_dim) # + '_e' + str(n_epoch)
-        new_model_dir = self.model_dir + model_name + '/'
-        if not os.path.exists(new_model_dir):
-            os.makedirs(new_model_dir)
+
+        if not os.path.exists(self.model_dir):
+            os.makedirs(self.model_dir)
 
         # clear tmp directory to save disk space
         logdir = "/tmp/tflearn_logs/"
         if clear_tmp == True and os.path.exists(logdir): 
             os.system("rm -r " + logdir + "*")
 
-        model_path = new_model_dir + model_name + '.h5'
+        model_path = os.path.join(self.model_dir, '{}.h5'.format(model_name))
         tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1)
         checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(filepath=model_path, verbose=1, save_best_only=True)
 
@@ -121,13 +134,42 @@ class ApplePicking:
             self.model = self.perf_nnet_conv1D(feature_dim, model_path)
         else:
             self.model = keras.models.load_model(model_path)
+            return
 
         (train_inputs, train_labels) = self.train
         (test_inputs, test_labels) = self.test
         self.model.fit(train_inputs, train_labels, epochs=n_epoch, verbose=1, callbacks=[tensorboard_cb, checkpoint_cb], validation_data=(test_inputs, test_labels), shuffle=True)
 
-def main():
-    apple_picking_obj = ApplePicking()
-    apple_picking_obj.train_network()
+
+
 if __name__ == '__main__':
-    main()
+    import sys
+    import matplotlib.pyplot as plt
+
+    mode = 'train'
+    if len(sys.argv) > 1:
+        mode = sys.argv[1]
+
+    apple_picking_obj = ApplePicking()
+
+    if mode == 'train':
+
+        apple_picking_obj.train_network(is_restore=False)
+
+    elif mode == 'validate':
+        apple_picking_obj.train_network(is_restore=True)
+        output_base = os.path.join(ROOT, 'output_{}_{}.png')
+
+        for data_label, dataset in [['Training', apple_picking_obj.train[0]], ['Validation', apple_picking_obj.test[0]]]:
+            # Test a data set
+            inputs, outputs = apple_picking_obj.train
+            predictions = apple_picking_obj.model.predict(inputs)
+
+
+            for idx, label in [(0, 'X'), (1, 'Y'), (2, 'Z')]:
+                plt.clf()
+                plt.plot(outputs[:, idx], label=label, color='r')
+                plt.plot(predictions[:, idx], label='{} (pred)'.format(label), color='g')
+                plt.legend()
+                plt.title('{} Data - {} coord'.format(data_label, label))
+                plt.savefig(output_base.format(data_label.lower(), label.lower()))
