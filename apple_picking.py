40185236bfaaa705a9d290810ef5d82f51b76038
prakash1895 <baskarap@oregonstate.edu>
2019-11-29 15:33:36 -0800

    New Data and Models trained on joint angles

--- apple_picking.py
+++ apple_picking.py
@@ -26,7 +26,9 @@ class ApplePicking:
         self.model_dir = os.path.join(ROOT, model_folder)
         
         self.window_size = window_size
-        self.INPUT_COLS = ['force_mag', '/wrench.fx', '/wrench.fy', '/wrench.fz', '/manipulator_pose.x', '/manipulator_pose.y', '/manipulator_pose.z', '/manipulator_pose.rx', '/manipulator_pose.ry', '/manipulator_pose.rz', '/manipulator_pose.rw']
+        self.INPUT_COLS = ['/manipulator_wrench.fx', '/manipulator_wrench.fy', '/manipulator_wrench.fz', '/manipulator_wrench.tx', '/manipulator_wrench.ty', '/manipulator_wrench.tz',  
+                           '/joint_states.shoulder_lift_joint', '/joint_states.elbow_joint', '/joint_states.wrist_1_joint', '/joint_states.wrist_2_joint', '/joint_states.wrist_3_joint']
+
         self.OUTPUT_COLS = ['/ground_truth.x', '/ground_truth.y', '/ground_truth.z']
         self.train, self.validation = self.load_all_data(val_split, is_smooth=False)
         
@@ -43,11 +45,11 @@ class ApplePicking:
         return f_mag
 
     def adjust_force_data(self, df):
-        force_cols = ['/wrench.fx', '/wrench.fy', '/wrench.fz']
+        force_cols = ['/manipulator_wrench.fx', '/manipulator_wrench.fy', '/manipulator_wrench.fz', '/manipulator_wrench.tx', '/manipulator_wrench.ty', '/manipulator_wrench.tz']
         subset = df[force_cols]
         tared = subset - subset.iloc[0]
         df[force_cols] = tared
-        df['force_mag'] = (tared ** 2).sum(axis=1) ** 0.5
+        # df['force_mag'] = (tared ** 2).sum(axis=1) ** 0.5
         df = df.iloc[1:].reset_index(drop=True)
         return df
 
@@ -73,16 +75,20 @@ class ApplePicking:
     def get_data(self, files_list, is_smooth):
         X_data = []
         Y_data = []
+        mode_col = '/mode./mode'
         for file in files_list:
             temp_df = pd.read_csv(file)
             temp_df = self.adjust_force_data(temp_df)
             
+            # Taking only the mode 3 columns
+            temp_df = temp_df.loc[temp_df[mode_col] == 3]
+            temp_df = temp_df.dropna().reset_index(drop=True)
+
             if is_smooth == True:
                 print("Smothing...")
                 temp_df = self.smooth_data(temp_df) # Smoothing using moving average filter
 
             temp_X, temp_Y = self.format_data(temp_df)
-            print("Formatting...")
             X_data = X_data + temp_X
             Y_data = Y_data + temp_Y
 
@@ -90,38 +96,38 @@ class ApplePicking:
         Y_data = np.array(Y_data, dtype=np.float64)
         return X_data, Y_data
 
-    def load_all_data(self, test_split, is_smooth=False):
+    def load_all_data(self, val_split, is_smooth=False):
         print ("Loading Data ...")
 
         all_files = [file for file in glob.glob(os.path.join(self.train_dir, "*.csv"), recursive=False)]
         random.shuffle(all_files)
 
-        split_idx = int(round(test_split*len(all_files)))
+        split_idx = int(round(val_split*len(all_files)))
         
-        test_files = all_files[0: split_idx]
+        val_files = all_files[0: split_idx]
         train_files = all_files[split_idx: ]
         
         train_X, train_Y = self.get_data(train_files, is_smooth)
-        test_X, test_Y = self.get_data(test_files, is_smooth)
+        val_X, val_Y = self.get_data(val_files, is_smooth)
 
         print("\nNumber of Train fies: ", len(train_files))
         print("Train data shape: {}, {}\n".format(train_X.shape, train_Y.shape))
-        print("Number of Test fies: ", len(test_files))
-        print("Test data shape: {}, {}\n".format(test_X.shape, test_Y.shape))
-        return (train_X, train_Y), (test_X, test_Y)
+        print("Number of Val fies: ", len(val_files))
+        print("Test data shape: {}, {}\n".format(val_X.shape, val_Y.shape))
+        return (train_X, train_Y), (val_X, val_Y)
 
     def perf_nnet_conv1D(self, feature_dim, output_dim, model_path):
         model = Sequential()
         model.add(Conv1D(32, 3, activation='relu', input_shape=(self.window_size, feature_dim)))
         model.add(Conv1D(64, 3, activation='relu'))
         # model.add(MaxPooling1D(2))
-        model.add(Dropout(0.3))
+        model.add(Dropout(0.2))
 
         model.add(Conv1D(128, 3, activation='relu'))
         # model.add(Conv1D(128, 3, activation='relu'))
         model.add(MaxPooling1D(2))
         # model.add(GlobalAveragePooling1D())
-        model.add(Dropout(0.3))
+        model.add(Dropout(0.2))
 
         model.add(Flatten())
         model.add(Dense(output_dim, activation='linear'))
@@ -154,7 +160,7 @@ class ApplePicking:
         if clear_tmp == True and os.path.exists(logdir): 
             os.system("rm -r " + logdir + "*")
 
-        model_name = 'force_vec_pred_ws' + str(self.window_size) + '_fdim' + str(feature_dim) + '_' + net # + '_e' + str(n_epoch)
+        model_name = 'force_vec_pred_' + net + '_ws' + str(self.window_size) + '_fdim' + str(feature_dim)  # + '_e' + str(n_epoch)
         model_path = os.path.join(self.model_dir, '{}.h5'.format(model_name))
         tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1)
         checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(filepath=model_path, verbose=1, save_best_only=True)
@@ -184,20 +190,34 @@ class ApplePicking:
         predictions = self.model.predict(inputs)
         return predictions
 
+    def orientation_error(self, targets, predictions):
+        orientation_error = []
+        for i in range(len(predictions)):
+            vec_1 = predictions[i]
+            vec_1 = vec_1/np.linalg.norm(vec_1)
+
+            vec_2 = targets[i]
+            vec_2 = vec_2/np.linalg.norm(vec_2)
+            radian = np.arccos(np.clip(np.dot(vec_1, vec_2), -1.0, 1.0))
+            theta = (180.0/math.pi)*radian
+            orientation_error.append(theta)
+        print(np.shape(orientation_error))
+        return orientation_error
+
 if __name__ == '__main__':
     
     mode = 'train_mode'
     if len(sys.argv) > 1:
         mode = sys.argv[1]
 
-    apple_picking_obj = ApplePicking(window_size=1)
+    apple_picking_obj = ApplePicking(window_size=10)
 
     if mode == 'train_mode':
-        apple_picking_obj.train_network(net='ANN', is_restore=False)
+        apple_picking_obj.train_network(net='Conv1D', is_restore=False)
 
     elif mode == 'predict_mode':
         # model_name = 'force_vec_pred_ws10_fdim11_Conv1D'
-        model_name =  'force_vec_pred_ws1_fdim11_ANN'
+        model_name =  'force_vec_pred_Conv1D_ws10_fdim11'
 
         output_base = os.path.join(ROOT, 'output_{}_{}.png')
 
@@ -210,16 +230,35 @@ if __name__ == '__main__':
             inputs, outputs = apple_picking_obj.validation
 
         inputs, outputs = shuffle(inputs, outputs, random_state=0)
-        predictions = apple_picking_obj.predict_network('ANN', model_name, inputs)
+        predictions = apple_picking_obj.predict_network('Conv1D', model_name, inputs)
+        orientation_error = apple_picking_obj.orientation_error(outputs, predictions)
+        
+        orientation_arr = np.array(orientation_error)
+        print("Maximum Ang Error: ", np.max(orientation_arr))
+        print("Minimum Ang Error: ", np.min(orientation_arr))
+        print("Average Ang Error: ", np.mean(orientation_arr))
+        print("Std Deviation Ang: ", np.std(orientation_arr))
 
+        # Plotting the error in the orientation
+        plt.clf()
+        plt.plot(orientation_error, color='r')
+        
+        plt.title('Orientation Error in degrees', fontsize=35)
+        plt.xlabel("Time Steps (0.1s)", fontsize=25)
+        plt.ylabel("Orrientation Error (deg)", fontsize=25)
+        plt.ylim(0.0, 20.0)
+        plt.show()
+        # plt.savefig(output_base.format(data_label.lower(), label.lower()))
+        
+        # Plotting individual X,Y,Z components
         for idx, label in [(0, 'X'), (1, 'Y'), (2, 'Z')]:
             plt.clf()
             plt.plot(outputs[:, idx], label=label, color='r')
             plt.plot(predictions[:, idx], label='{} (pred)'.format(label), color='g')
             plt.legend()
             plt.title('{} Data - {} coord'.format(data_label, label))
-            plt.savefig(output_base.format(data_label.lower(), label.lower()))
-            plt.show()
+            # plt.savefig(output_base.format(data_label.lower(), label.lower()))
+            # plt.show()
 
         # for data_label, dataset in [['Training', random.choice(apple_picking_obj.train)], ['Validation', random.choice(apple_picking_obj.test)]]:
         #     # Test a data set
